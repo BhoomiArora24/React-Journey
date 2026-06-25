4680e35a5308397d461b11db518df2f1c377f3a6
NavyaArora24 <aroranavya78@gmail.com>
2026-06-25 21:57:23 +0530

    props start

---
+++ ReactJourney/3props/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="h-auto text-5xl text-black border-2px border-solid border-black">
+      <h1>Navya Arora</h1>
+    </div>
+  )
+}
+
+export default App
